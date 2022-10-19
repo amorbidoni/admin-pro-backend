0c43b3e2932ef6c2eb739837057ddd31e7524d38
amorbidoni <86751120+amorbidoni@users.noreply.github.com>
2022-10-19 20:35:30 -0300

    ssl fix

--- database/config.js
+++ database/config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const dbConection = async () => {
   try {
-    await mongoose.connect(process.env.DB_CNN, {ssl:false});
+    await mongoose.connect(process.env.DB_CNN, {});
     console.log('DB online');
   } catch (error) {
     console.log(error);
